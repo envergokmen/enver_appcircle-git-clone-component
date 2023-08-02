f668aa80cb4592e9f8057d23458d3b465fcb16ec
Enver GÖKMEN <envergokmen@gmail.com>
2023-08-02 17:50:58 +0300

    extra header error fix

--- git_clone.sh
+++ git_clone.sh
@@ -114,8 +114,8 @@ fi
         runCommand git config remote.origin.lfspushurl "${GIT_URL}/info/ls"
     fi
     
-    git config --global --add http.http://:OjJiMms2MmVrb3JsaGx6bXplM2lld3hhZjVta3dyNGlqYmdvb3pvNHplcXY0aTV3cGwyNWE=@192.168.1.13:8989/DefaultCollection/Appcircle/_git/appcircle-sample-android.extraHeader "Authorization: Basic Om1sM253eWt5ZGtpam42M2pnaGNxeTJtd3Z2djY3ZjUyZzRibng0cTVndWZteGtmcW1la3E="
-    
+    git config --global --add http.http://DefaultCollection:OjJiMms2MmVrb3JsaGx6bXplM2lld3hhZjVta3dyNGlqYmdvb3pvNHplcXY0aTV3cGwyNWE=@192.168.1.13:8989/DefaultCollection/Appcircle/_git/appcircle-sample-android.extraHeader "AUTHORIZATION: Basic Om1sM253eWt5ZGtpam42M2pnaGNxeTJtd3Z2djY3ZjUyZzRibng0cTVndWZteGtmcW1la3E="
+    git config --list | grep http.
     #if [ ! -z "${GIT_EXTRA_PARAMS}" ] ; then
        # GIT_URL_FOR_EXTRA_PARAM="${GIT_URL%.git}"
        #runCommand echo "GIT_URL_FOR_EXTRA_PARAM -> ${GIT_URL_FOR_EXTRA_PARAM}"
