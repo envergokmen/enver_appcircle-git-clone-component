bc647d9c071849e8328706d34a978ff44bf2cc3a
Enver GÖKMEN <envergokmen@gmail.com>
2023-08-02 15:32:01 +0300

    string error fix

--- git_clone.sh
+++ git_clone.sh
@@ -115,10 +115,9 @@ fi
     fi
     
     if [ ! -z "${GIT_EXTRA_PARAMS}" ] ; then
-
-       GIT_URL_FOR_EXTRA_PARAM="${GIT_URL%.git}"
-       runCommand echo "GIT_URL_FOR_EXTRA_PARAM -> ${GIT_URL_FOR_EXTRA_PARAM}"
-       runCommand git config --global --add http.${GIT_URL_FOR_EXTRA_PARAM}.extraHeader ${GIT_EXTRA_PARAMS}
+        GIT_URL_FOR_EXTRA_PARAM="${GIT_URL%.git}"
+       #runCommand echo "GIT_URL_FOR_EXTRA_PARAM -> ${GIT_URL_FOR_EXTRA_PARAM}"
+       runCommand git config --local --add http."${GIT_URL_FOR_EXTRA_PARAM}".extraHeader "${GIT_EXTRA_PARAMS}"
     fi
 
     fill
